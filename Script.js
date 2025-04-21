e42cb441a8f2047cc69e16d04bfb2d1164ae9b7d
hamza-boody <boodyhamzo@gmail.com>
2025-04-21 15:19:49 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,205 @@
+// let object={
+//     name:"hamza"
+//     ,age:10
+//     ,grad:"2nd"
+// }
+// console.log(object.name)
+
+
+// let x= Number (prompt("Enter Your First Number,please")) 
+
+
+// let y= Number (prompt("Enter Your Second Number,please")) 
+
+
+// let w=  prompt("Enter Your Operation,please")
+
+// switch(w){
+
+// case"/": document.write(x/y)
+
+// break;
+
+// case"x": document.write(x*y) 
+
+
+
+// break;
+
+// case"-": document.write(x-y)
+
+// break;
+
+// case"+": document.write(x+y)
+
+// break;
+
+// case"*":document.write(x*y) 
+
+// break;
+
+// case"%": document.write(x/y)
+
+// break;
+
+
+
+// default: 
+//          document.write("YOU HAVE AN ERROR IN YOUR CALCULATIONS")
+
+
+
+// }
+//  
+
+
+
+
+
+
+
+
+
+let b=prompt("Enter your name(Hamza)")
+
+
+
+
+
+let object={
+         name:"Hamza",
+         maris:function (){document.write()
+            if(b === "hamza"){
+            document.write("Welcome to the website.")
+            }
+            else if(!b){ document.write("Please enter your name.")}
+         }
+          }
+
+          object.maris()
+// let names={
+//     name:"Hamza",
+//     names:"Mohamed",
+//     namess:"Ahmed",  
+    
+//     namesss:"Omar"
+//   , z:function () {
+    
+//   }
+    
+// }
+
+
+// console.log("hello,"+b)
+
+
+
+// if(b.trim()===""){console.log("Please enter your name.")}
+
+
+
+
+
+// switch(b){
+
+// case b: console.log(`hello ,`+b)
+
+// break;
+
+
+
+// default:console.log( "Please")
+
+
+
+
+
+
+
+
+
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+// function nameWords(prompt) {
+//     const words = prompt.split(' ');
+//     const namedWords = {};
+//     words.forEach((word, index) => {
+//       namedWords[`Word${index + 1}`] = word;
+//     });
+//     return namedWords;
+//   }
+  
+//   const prompt = "Extract and name the words inside the prompt";
+//   const result = nameWords(prompt);
+//   console.log(result);
+
+
+
+
+// let object={name:hamza
+
+//     maris
+// }
+
+ 
+// for(let values in objects)
+//     {document.write(objects[values])}
+
+
+// for(let key in objects)
+//     {document.write(`${key}`)}
+
+
+
+
+// for(let key in objects)
+//     {console.log(`${key}=${objects[key]}`)}
+
+
+
+// let values =Object.values(objects)
+
+// for(let i=0;i<values.length;i++){
+//     console.log(`${values[i]}`)
+// }
+
+
+
+
+
+
+
+
+// for(let i=0;i<grades.length;i++){console.log(grades[i])}
+// console.log(xray[0])
+
+
+// let ray=["ahmed",[1,2,3]]
+
+// console.log(ray[1][2])
+// let grades=["20%","40%","60%","80%","100%"]
+
+
+
+
+// grades.unshift("10%")
+// console.log(grades)
+// if (condition1) {
+//     //  block of code to be executed if condition1 is true
+//   } else if (condition2) {
+//     //  block of code to be executed if the condition1 is false and condition2 is true
+//   } else {
+//     //  block of code to be executed if the condition1 is false and condition2 is false
+//   }
